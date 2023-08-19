bad922c6ddf6e1d2fd43ecbc3a0679dc5eeb5fa1
Shawn Robinson <shawnzr23@gmail.com>
2023-08-18 23:58:17 -0400

    test.1

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = ""; // Paste your API key from OpenAI here
+const API_KEY = "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"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
+  // Create a chat <li> element with passed message and className
   const chatLi = document.createElement("li");
   chatLi.classList.add("chat", `${className}`);
   let chatContent =
@@ -17,7 +18,7 @@ const createChatLi = (message, className) => {
       : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
   chatLi.innerHTML = chatContent;
   chatLi.querySelector("p").textContent = message;
-  return chatLi;
+  return chatLi; // return chat <li> element
 };
 
 const generateResponse = (chatElement) => {
@@ -29,7 +30,7 @@ const generateResponse = (chatElement) => {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${API_KEY}`,
+      Authorization: "Bearer ${API_KEY}:",
     },
     body: JSON.stringify({
       model: "gpt-3.5-turbo",
@@ -37,6 +38,7 @@ const generateResponse = (chatElement) => {
     }),
   };
 
+  // Send POST request to API, get response and set the reponse as paragraph text
   fetch(API_URL, requestOptions)
     .then((res) => res.json())
     .then((data) => {
@@ -51,16 +53,19 @@ const generateResponse = (chatElement) => {
 };
 
 const handleChat = () => {
-  userMessage = chatInput.value.trim();
+  userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
   if (!userMessage) return;
 
+  // Clear the input textarea and set its height to default
   chatInput.value = "";
   chatInput.style.height = `${inputInitHeight}px`;
 
+  // Append the user's message to the chatbox
   chatbox.appendChild(createChatLi(userMessage, "outgoing"));
   chatbox.scrollTo(0, chatbox.scrollHeight);
 
   setTimeout(() => {
+    // Display "Thinking..." message while waiting for the response
     const incomingChatLi = createChatLi("Thinking...", "incoming");
     chatbox.appendChild(incomingChatLi);
     chatbox.scrollTo(0, chatbox.scrollHeight);
@@ -69,11 +74,14 @@ const handleChat = () => {
 };
 
 chatInput.addEventListener("input", () => {
+  // Adjust the height of the input textarea based on its content
   chatInput.style.height = `${inputInitHeight}px`;
   chatInput.style.height = `${chatInput.scrollHeight}px`;
 });
 
 chatInput.addEventListener("keydown", (e) => {
+  // If Enter key is pressed without Shift key and the window
+  // width is greater than 800px, handle the chat
   if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
     e.preventDefault();
     handleChat();
