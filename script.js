d13add7ba5506b1a7c67c5d24e426b7e1df7859f
Shawn Robinson <shawnzr23@gmail.com>
2023-08-26 16:56:18 -0400

    update

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = ""; // Enter Your Own API Key Here
+const API_KEY = ""; // Enter Your Own API Key Here (Go To OpenAIxs)
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
